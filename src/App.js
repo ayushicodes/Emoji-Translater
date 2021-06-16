c304f36c257c8a519acd83c447775c7fb1fb687d
ayushicodes <raiayushi12911@gmail.com>
2021-06-16 18:41:11 +0530

    project completed

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import './App.css';
+import React, { useState } from "react";
+
+// import Hello from './Hello'
+
+var emojiDictionary = {
+
+  "😀": " Grinning Face",
+  "😉": "Flirting With Eyes",
+  "😘": "Chummi",
+  "😜": "Let Me Lick It",
+  "🤑": " Paisa Paisa",
+  "💩": "Pile of Poo",
+  "🖕": "Fuck You",
+  "👩‍💻": "Coder Ayushi",
+  "👨‍👩‍👧": "Ayushi Charu Ayra",
+  "🦈": "Chandra Mohan Pandey",
+  "🌿": " Gaanja",
+  "😒": "Huh!! Kuch Bhi",
+  "🙄": "Kya Bol Rhe Ho",
+  "🥱": "Kya Paka Rha Hai",
+  "🤬": "Chla Ja Bsdk",
+  "☠️": "Bhoot Bna Dunga",
+  "👸": "Ayushi",
+  "🦸‍♂️": "Crime Master Gogo",
+  "👩‍❤️‍👨 ": "OYO",
+  "👨‍👩‍👧‍👦": "Hum Do Hmare Do ",
+  "😄": "Chl Jutha",
+  "🤪": "Baklol",
+  "🤫": "Chup Re",
+  "🤔": "Konsi Gali Du",
+  "😑": "Abe Ja Na",
+  "😔": "Dil Se Bura Lgta Hai",
+  "😷": "China ki _____",
+  "🤮": "Muh P Ulti Kr Dunga",
+  "🤯": "Dimag Phat Gya",
+  "😟": "Abe Yrrr",
+  "😲": "Ye Kya Dekh Liya",
+  "🥺": "Man Ja Pls",
+  "😈": "Rhea Chakraborty",
+  "💋": "Chummi Max Pro",
+  "🤚": "Congress",
+  "🌷": "BJP",
+  "👊": "Mukka Khana Hai",
+  "🙏": "Tu Jita",
+  "🦵": "Leg Piece Bna Denge",
+  "👃": "Lord Voldemort",
+  "👋": "Ek Kantap P Denge"
+
+};
+var emojisWeKnow = Object.keys(emojiDictionary)
+
+export default function App() {
+  const [meaning, setMeaning] = useState("")
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+    var meaning = emojiDictionary[userInput];
+    if (meaning === undefined) {
+      meaning = "We dont have this in Our database"
+    }
+    setMeaning(meaning)
+  }
+  function emojiClickHandler(emoji) {
+    let meaning = emojiDictionary[emoji]
+    setMeaning(meaning)
+  }
+  return (
+    <div className="App">
+      <h1>INSIDE OUT</h1>
+
+      <input placeHolder="Put Your Emojis Here!!!" className="inputbox" onChange={emojiInputHandler} />
+      <h2> {meaning} </h2>
+      <h3> Emojis We Know </h3>
+      {emojisWeKnow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer", paddingLeft: "50px", paddingRight: "50px" }}
+            key={emoji}
+          >
+            {emoji}
+          </span>
+        );
+      })}
+      <footer></footer>
+    </div>
+  )
+}
---
+++ src/Hello.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Hello(props) {
+    return (
+        <p>Hello! {props.name}! how are you? {props.age}</p>
+    )
+}
+
+export default Hello
